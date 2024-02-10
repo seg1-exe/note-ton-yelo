68bbc352f35f6eb64ed8f7dd514803984457c06a
Rutabagarre <rutab9ga@gmail.com>
2024-02-10 19:12:33 +0100

    Javascript terminé

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Note ton Yelo !
 
 *en cours de développement*
+
+-- npm install url-parse
+
+inputSubmit.addEventListener('click', () => {
+ 
+
+  //Vérif si les inputs ne sont pas vides
+  if(!bikeInput.value == "") {
+    
+    //Véficitaion de la validité de l'identifiant du vélo
+    if(!bikeInput.value == "" && bikeInput.value > 1000 && bikeInput.value < 9999){
+      const idOfBike = bikeInput.value;
+
+      bikeOutput.innerHTML = idOfBike;
+      
+    } else {
+      alert("Merci de renseigner un identifiant de vélo valide.");
+    }  
+  } else {
+    //Si les inputs sont vides
+    alert("Merci de renseigner un lien valide ou un identifiant de vélo.");
+  }
+});
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import parse from 'url-parse';
+
 const yeloInput = document.getElementById('yelo-input');
 const bikeInput = document.getElementById('bike-input');
 const inputSubmit = document.getElementById('input-submit');
@@ -9,28 +11,57 @@ const radiosButtons = document.querySelectorAll('input[name="rating"]');
 const ratingSubmit = document.getElementById('rating-submit');
 const ratingOutput = document.getElementById('rating-output');
 
+
+
 inputSubmit.addEventListener('click', () => {
-  //url = https://freebike.app.link/?bikeId=0000&system=larochelle
-  if(!yeloInput.value == "" || !bikeInput.value == "") {
-    if(bikeInput.value > 1000 && bikeInput.value < 9999){
-      const idOfBike = bikeInput.value;
-      bikeOutput.innerHTML = idOfBike;
-    } else {
-      alert("Merci de renseigner un identifiant de vélo valide.");
-    }
 
-    if(!yeloInput.value == ""){
-      const url = yeloInput.value;
+  if (yeloInput.value == "" && bikeInput.value == ""){
+    //Si les deux inputs sont vides
+    alert("Merci de renseigner un lien ou un numéro de vélo.");
+  } else if (!yeloInput.value == ""){
+    //urltype = https://freebike.app.link/?bikeId=5131&system=larochelle
+    const urlMere = "https://freebike.app.link/?bikeId=0000&system=larochelle";
+    const url = yeloInput.value;
+
+    const parsedUrlMere = parse(urlMere);
+    const urlInput = parse(url);
+
+    //On compare les deux urls pour vérifier qu'elles sont similaires
+    const areSimilar = parsedUrlMere.hostname === urlInput.hostname && parsedUrlMere.pathname === urlInput.pathname;
+    console.log(areSimilar);
+
+    //Si les urls sont similaires, on récupère l'id du vélo
+    if (areSimilar == true){
       const parts = url.split("?");
-      const query = parts[1].split("&");
-      const bikeId = query[0].split("=")[1];
-      bikeOutput.innerHTML = bikeId;
-    }
 
-    myBike.style.display = "block";
-  } else {
-    alert("Merci de renseigner un lien valide ou un identifiant de vélo.");
+      const query = parts[1].split("&");
+
+      const bikeId = query[0].split("=")[1];
+      console.log(bikeId);
+      
+      //On vérifie que l'id est bien compris entre 1000 et 9999
+      if (bikeId > 1000 && bikeId < 9999){
+        bikeOutput.innerHTML = bikeId;
+        myBike.style.display = "block";
+      } else {
+        alert("Merci de renseigner un numéro de vélo valide.");
+      }
+    } else { 
+      alert("Merci de renseigner un lien valide.");
+    }
+  } else if (!bikeInput.value == ""){
+    const bikeId = bikeInput.value;
+
+    //On vérifie que l'id est bien compris entre 1000 et 9999
+    if (bikeId > 1000 && bikeId < 9999){
+      bikeOutput.innerHTML = bikeId;
+      myBike.style.display = "block";
+    } else {
+      alert("Merci de renseigner un numéro de vélo valide.");
+    }
   }
+
+   
 });
 
 ratingSubmit.addEventListener('click', () => {
@@ -38,9 +69,10 @@ ratingSubmit.addEventListener('click', () => {
   for (const radioButton of radiosButtons) {
       if (radioButton.checked) {
           ratingSelected = radioButton.value;
+          ratingOutput.innerHTML = ratingSelected;
           break;
+      } else {
+          ratingOutput.innerHTML = "Merci de renseigner une note.";
       }
   }
-
-  ratingOutput.innerHTML = ratingSelected;
 });
