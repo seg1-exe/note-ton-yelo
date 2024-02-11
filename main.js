684d2e880241044e2c085344ffa7ae6c6132ec71
Rutabagarre <rutab9ga@gmail.com>
2024-02-11 19:51:01 +0100

    import module parse corrigé + redirection direct après scan

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { parse } from 'url-parse';
+import parse from 'url-parse';
 import { Html5Qrcode, Html5QrcodeScanner } from 'html5-qrcode';
 
 const yeloInput = document.getElementById('yelo-input');
@@ -21,38 +21,6 @@ inputSubmit.addEventListener('click', () => {
   if (yeloInput.value == "" && bikeInput.value == ""){
     //Si les deux inputs sont vides
     alert("Merci de renseigner un lien ou un numéro de vélo.");
-  } else if (!yeloInput.value == ""){
-    //urltype = https://freebike.app.link/?bikeId=5131&system=larochelle
-    const urlMere = "https://freebike.app.link/?bikeId=0000&system=larochelle";
-    const url = yeloInput.value;
-
-    const parsedUrlMere = parse(urlMere);
-    const urlInput = parse(url);
-
-    //On compare les deux urls pour vérifier qu'elles sont similaires
-    const areSimilar = parsedUrlMere.hostname === urlInput.hostname && parsedUrlMere.pathname === urlInput.pathname;
-    console.log(areSimilar);
-
-    //Si les urls sont similaires, on récupère l'id du vélo
-    if (areSimilar == true){
-      const parts = url.split("?");
-
-      const query = parts[1].split("&");
-
-      const bikeId = query[0].split("=")[1];
-      console.log(bikeId);
-      
-      //On vérifie que l'id est bien compris entre 1000 et 9999
-      if (bikeId > 1000 && bikeId < 9999){
-        bikeOutput.innerHTML = bikeId;
-        myBike.style.display = "flex";
-        inputArea.style.display = "none";
-      } else {
-        alert("Merci de renseigner un numéro de vélo valide.");
-      }
-    } else { 
-      alert("Merci de renseigner un lien valide.");
-    }
   } else if (!bikeInput.value == ""){
     const bikeId = bikeInput.value;
 
@@ -65,8 +33,6 @@ inputSubmit.addEventListener('click', () => {
       alert("Merci de renseigner un numéro de vélo valide.");
     }
   }
-
-   
 });
 
 ratingSubmit.addEventListener('click', () => {
@@ -99,6 +65,40 @@ scanner.render(success, error);
 function success(data) {
   console.log(data);
   yeloInput.value = data;
+
+  if (!yeloInput.value == ""){
+    //urltype = https://freebike.app.link/?bikeId=5131&system=larochelle
+    const urlMere = "https://freebike.app.link/?bikeId=0000&system=larochelle";
+    const url = yeloInput.value;
+
+    const parsedUrlMere = parse(urlMere);
+    const urlInput = parse(url);
+
+    //On compare les deux urls pour vérifier qu'elles sont similaires
+    const areSimilar = parsedUrlMere.hostname === urlInput.hostname && parsedUrlMere.pathname === urlInput.pathname;
+    console.log(areSimilar);
+
+    //Si les urls sont similaires, on récupère l'id du vélo
+    if (areSimilar == true){
+      const parts = url.split("?");
+
+      const query = parts[1].split("&");
+
+      const bikeId = query[0].split("=")[1];
+      console.log(bikeId);
+      
+      //On vérifie que l'id est bien compris entre 1000 et 9999
+      if (bikeId > 1000 && bikeId < 9999){
+        bikeOutput.innerHTML = bikeId;
+        myBike.style.display = "flex";
+        inputArea.style.display = "none";
+      } else {
+        alert("Merci de renseigner un numéro de vélo valide.");
+      }
+    } else { 
+      alert("Merci de renseigner un lien valide.");
+    }
+  }
 
   scanner.clear();
 }
